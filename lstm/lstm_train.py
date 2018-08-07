23c412781727b96fb2cfac99e2cbd05f81fa1707
fanfanda <fanfanda@ict.ac.cn>
2018-08-07 20:11:01 +0800

    nothing

--- lstm/lstm_train.py
+++ lstm/lstm_train.py
@@ -92,7 +92,7 @@ def create_dictionaries(model=None,
         combined= sequence.pad_sequences(combined, maxlen=maxlen)#每个句子所含词语对应的索引，所以句子中含有频数小于10的词语，索引为0
         return w2indx, w2vec,combined
     else:
-       print 'No data provided...'
+       print('No data provided...')
 
 
 #创建词语字典，并返回每个词语的索引，词向量，以及每个句子所对应的词语索引
@@ -125,7 +125,7 @@ def get_data(index_dict,word_vectors,combined,y):
 
 ##定义网络结构
 def train_lstm(n_symbols,embedding_weights,x_train,y_train,x_test,y_test):
-    print 'Defining a Simple Keras Model...'
+    print('Defining a Simple Keras Model...')
     model = Sequential()  # or Graph or whatever
     model.add(Embedding(output_dim=vocab_dim,
                         input_dim=n_symbols,
@@ -137,14 +137,14 @@ def train_lstm(n_symbols,embedding_weights,x_train,y_train,x_test,y_test):
     model.add(Dense(3, activation='softmax')) # Dense=>全连接层,输出维度=3
     model.add(Activation('softmax'))
 
-    print 'Compiling the Model...'
+    print('Compiling the Model...')
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam',metrics=['accuracy'])
 
-    print "Train..." # batch_size=32
+    print("Train...") # batch_size=32
     model.fit(x_train, y_train, batch_size=batch_size, epochs=n_epoch,verbose=1)
 
-    print "Evaluate..."
+    print("Evaluate...")
     score = model.evaluate(x_test, y_test,
                                 batch_size=batch_size)
 
@@ -152,20 +152,20 @@ def train_lstm(n_symbols,embedding_weights,x_train,y_train,x_test,y_test):
     with open('../model/lstm.yml', 'w') as outfile:
         outfile.write( yaml.dump(yaml_string, default_flow_style=True) )
     model.save_weights('../model/lstm.h5')
-    print 'Test score:', score
+    print('Test score:', score)
 
 
 #训练模型，并保存
-print 'Loading Data...'
+print('Loading Data...')
 combined,y=loadfile()
-print len(combined),len(y)
-print 'Tokenising...'
+print(len(combined),len(y))
+print('Tokenising...')
 combined = tokenizer(combined)
-print 'Training a Word2vec model...'
+print('Training a Word2vec model...')
 index_dict, word_vectors,combined=word2vec_train(combined)
 
-print 'Setting up Arrays for Keras Embedding Layer...'
+print('Setting up Arrays for Keras Embedding Layer...')
 n_symbols,embedding_weights,x_train,y_train,x_test,y_test=get_data(index_dict, word_vectors,combined,y)
-print "x_train.shape and y_train.shape:"
-print x_train.shape,y_train.shape
+print("x_train.shape and y_train.shape:")
+print(x_train.shape,y_train.shape)
 train_lstm(n_symbols,embedding_weights,x_train,y_train,x_test,y_test)
